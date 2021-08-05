8ecf10f0453999fc1af76b01d9794bd234591496
Janley Crsiologo <crisoja@oocl.com>
2021-08-05 20:18:38 +0800

    [MarsRover][Janley] test: rename should_return_locationX_0_locationY_1_direction_E_when_execute_command_given_locationX_0_locationY_0_direction_S_and_L

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -65,7 +65,7 @@ class MarsRoverTest {
     }
 
     @Test
-    void should_return_locationX_0_locationY_negative_1_direction_E_when_execute_command_given_locationX_0_locationY_0_direction_S_and_L() {
+    void should_return_locationX_0_locationY_1_direction_E_when_execute_command_given_locationX_0_locationY_0_direction_S_and_L() {
         //given
         MarsRover marsRover = new MarsRover(new RoverStatus(0,0,"S"));
         RoverStatus expectedRoverStatus = new RoverStatus(0,0,"E");
