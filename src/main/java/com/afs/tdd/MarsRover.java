a7343f5f34f465c1e7a0a35a32da456b7a5f5d7d
Janley Crsiologo <crisoja@oocl.com>
2021-08-05 17:46:05 +0800

    [MarsRover][Janley] should_return_locationX_0_locationY_1_direction_N_when_execute_command_given_locationX_0_locationY_0_direction_N_and_M

---
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -0,0 +1,12 @@
+package com.afs.tdd;
+
+public class MarsRover {
+    RoverStatus roverStatus;
+    public MarsRover(RoverStatus roverStatus) {
+        this.roverStatus = roverStatus;
+    }
+
+    public RoverStatus getRoverStatus() {
+        return null;
+    }
+}
---
+++ src/main/java/com/afs/tdd/RoverStatus.java
@@ -0,0 +1,15 @@
+package com.afs.tdd;
+
+public class RoverStatus {
+    public RoverStatus(int locationX, int locationY, String direction) {
+
+    }
+
+    public int getLocationX() {
+        return 0;
+    }
+
+    public int getLocationY() {
+        return 0;
+    }
+}
